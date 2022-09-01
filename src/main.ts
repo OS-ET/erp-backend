ad2af94f0acf8a835660c3065f27c3b4be73cfb9
Abdulkerim Jemal <abduljemal43@gmail.com>
2022-09-01 10:17:41 +0300

    Security Packages added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(csurf())
+  app.use(helmet())
   await app.listen(3000);
 }
 bootstrap();
