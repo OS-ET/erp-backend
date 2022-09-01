b26f39570eb5ab74631afd55203c23fc871bcceb
Abdulkerim Jemal <abduljemal43@gmail.com>
2022-09-01 12:07:50 +0300

    Environment Configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number(),
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as csurf from 'csurf';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,15 +11,16 @@ async function bootstrap() {
   app.use(helmet())
   //TODO: enable csurf after initializing session or cookie parser
   // app.use(csurf()) 
-  const config = new DocumentBuilder()
-  .setTitle("Open Source ERP project")
-  .setDescription("Ethiopian Open source projects")
-  .setVersion('1.0')
-  .addTag('OCS-backend')
-  .build();
-  const document=SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('api',app,document)
-
-  await app.listen(3000);
+  if (app.get(ConfigService).get('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle("Open Source ERP project")
+      .setDescription("Ethiopian Open source projects")
+      .setVersion('1.0')
+      .addTag('OCS-backend')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document)
+  }
+  await app.listen(app.get(ConfigService).get<number>('PORT') || 3000);
 }
 bootstrap();
