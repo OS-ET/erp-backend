594b6c20d021ad3f1dad76f17e1b8469a8dbbcae
Abdulkerim Jemal <abduljemal43@gmail.com>
2022-09-01 10:35:38 +0300

    Open Api - swagger added

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(csurf())
   app.use(helmet())
+  //TODO: enable csurf after initializing session or cookie parser
+  // app.use(csurf()) 
+  const config = new DocumentBuilder()
+  .setTitle("Open Source ERP project")
+  .setDescription("Ethiopian Open source projects")
+  .setVersion('1.0')
+  .addTag('OCS-backend')
+  .build();
+  const document=SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document)
+
   await app.listen(3000);
 }
 bootstrap();
